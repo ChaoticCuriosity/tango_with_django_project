942b2b364da8f956329e901551ddfde74dd6cac9
Christopher Cookman <2145938c@student.gla.ac.uk>
2017-02-02 14:05:34 +0000

    Chapter 6 complete

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,6 @@ from rango import views
 urlpatterns = [
 
     url(r'^about/$', views.about, name='about'),
-	url(r'', views.index, name = 'index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='category'),
+	url(r'', views.index, name = 'show_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import page
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages'; page_list}
+    #context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context_dict)
     #Html = "Rango says hey there partner! <a href='/rango/about/'>About</a><br />"
     #return HttpResponse(Html)
 
@@ -14,5 +19,24 @@ def about(request):
 
     #Html = "Rango says welcome to the about page! <a href='/rango/'>Index</a><br />"
     #return HttpResponse(Html)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict[ 'pages' ] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 
 
